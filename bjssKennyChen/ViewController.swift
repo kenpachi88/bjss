b097fa33586f3dad2c8b8a41805c1a2368145038
kenny <kenny.chen@live.com>
2020-01-22 08:09:04 -0500

    Initial Commit

---
+++ bjssKennyChen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  bjssKennyChen
+//
+//  Created by NYCDOE on 1/22/20.
+//  Copyright © 2020 hireMeKennyChen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
